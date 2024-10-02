09a7c60b65aea56f448f782f6c155f8ee55c92ed
PGL <77868747+Pamlez-Tec@users.noreply.github.com>
2024-10-01 19:05:18 -0600

    Se añaden las estructuras entry y userdata

--- src/request_handler.h
+++ src/request_handler.h
@@ -2,8 +2,29 @@
 #define REQUEST_HANDLER_H
 
 #include <string>
+#include <vector>
+
+#define NAME_LENGTH 50 //Longitud máxima para los nombres.
+#define EMAIL_LENGTH 50 //Longitud máxima para los correos electrónicos.
+
+//Estructura para almacenar una entrada de usuario
+struct Entry {
+    char name[NAME_LENGTH]; 
+    char email[EMAIL_LENGTH]; 
+};
+
+//Estructura para devolver los resultados
+struct UserData {
+    std::string name;
+    std::string email;
+    std::string updated_name;
+    std::string updated_email;
+};
+
+//Para los tests
+extern std::vector<Entry> entries; 
 
 std::string generate_session_id();
 std::string process_request(const std::string &request, const std::string &session_id);
 
-#endif
+#endif 
